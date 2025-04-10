8c4322fbb6298f71ee6e463d260d83c9ef08617d
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-10 16:35:59 +0200

    feat: bonus 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,32 @@ function App() {
 
   const [movies, setMovies] = useState(dataMovies);
   const [filterMovies, setFilterMovies] = useState('');
+  const [titleMovies, setTitleMovies] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(filterMovies);
     let result = dataMovies;
 
-    if(filterMovies !== ""){
+    if (filterMovies !== "") {
       result = dataMovies.filter(movie => movie.genre.includes(filterMovies));
     }
-    
+
     setMovies(result);
     console.log(result)
     result = dataMovies;
   }, [filterMovies]);
 
+  function searchTitle(e) {
+    e.preventDefault();
+    console.log(titleMovies);
+    let result = dataMovies;
+    if(titleMovies !== ""){
+      result = dataMovies.filter(movie => movie.title === titleMovies);
+    }
+    
+    setMovies(result);
+    setTitleMovies('');
+  }
 
   return (
     <>
@@ -27,7 +39,7 @@ function App() {
 
         <h3>Seleziona il genere</h3>
         <div className="my-4">
-          <select value={filterMovies} onChange={e => setFilterMovies(e.target.value)}>
+          <select value={filterMovies} onChange={e => setFilterMovies(e.target.value)} className="form-select">
             <option value="">---</option>
             <option>Fantascienza</option>
             <option>Thriller</option>
@@ -37,6 +49,7 @@ function App() {
         </div>
 
         <div className="container d-flex row gap-2">
+          <p className="text-center d-none">Non ci sono film con questo titolo</p>
           {movies.map((movies, index) =>
             <div key={index} className="bg-success d-flex justify-content-center gap-5">
               <p><span className="fw-bold">Titolo: </span>{movies.title}</p>
@@ -45,6 +58,18 @@ function App() {
           )}
         </div>
 
+        <form onSubmit={searchTitle} className="d-flex justify-content-center my-4 gap-1">
+          <div>
+            <input
+              type="text"
+              value={titleMovies}
+              onChange={(e) => setTitleMovies(e.target.value)}
+              className="form-control"
+              placeholder="titolo"
+            />
+          </div>
+          <button className="btn btn-outline-success">Cerca Titolo</button>
+        </form>
       </div>
     </>
   )
